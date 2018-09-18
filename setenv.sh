fbd6d88dc3c3d26a29bb30c6fd865c71e7c4d2f1
Mike Fulton <mikefultonpersonal@gmail.com>
2018-09-18 16:42:17 -0500

    Added in a .gitignore file to skip any accidental build files getting pushed and made changes so setenv.sh is correct for -this- branch (my TrivialChange)

--- setenv.sh
+++ setenv.sh
@@ -1,13 +1,12 @@
 #
 # Set up the mvscmd environment.
 # NOTE: Before running this script, you need to have modified this file to match your z/OS system
-# AND you need to have added ${AUTHHLQ}.${AUTHLD} to your LNKLST 
 #
 
 #
 # The following environment variables must be set up in order to install mvscmd
 #
-export MVSCOMMAND_ROOT=/u/ibmuser/MVSCommand   # Location where mvscmd installed
+export MVSCOMMAND_ROOT=${TOOLS_ROOT}/src/MVSCommand   # Location where mvscmd installed
 export CHLQ=CBC                                # High Level qualifier for C/C++ compiler datasets (e.g. ${CHLQ}.SCCNCMP is the compiler executable PDSE)
 export PATH=${MVSCOMMAND_ROOT}/bin:$PATH
 
@@ -15,7 +14,7 @@ export PATH=${MVSCOMMAND_ROOT}/bin:$PATH
 # The following environment variables are required if you want to develop and test mvscmd
 #
 
-export TESTHLQ=IBMUSER                         # High Level qualifier test datasets created under
+export TESTHLQ=TSTRADM                         # High Level qualifier test datasets created under
 export LEHLQ=CEE                               # High Level qualifier for LE datasets (SCEELKED, SCEELKEX, SCEERUN, SCEERUN2)
 export COBOLHLQ=IGY610                         # High Level qualifier for COBOL compiler datasets (SIGYCOMP)
 export PLIHLQ=IEL510                           # High Level qualifier for PL/I compiler datasets (SIBMZCMP)
