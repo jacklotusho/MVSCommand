5eff2ba1ec0deeea5c9278a871551be3294842f2
Mike Fulton <mikefultonpersonal@gmail.com>
2018-09-30 19:47:18 -0500

    changed runTests.sh to use the new mdiff utility and then started changing a couple test outputs to use regular expressions

--- runTests.sh
+++ runTests.sh
@@ -19,5 +19,5 @@ for test in ${tests}; do
 	echo ${test}
 	name="${test%.*}"
 	${test} >${name}.actual 2>&1
-	diff -w ${name}.actual ${name}.expected
+	mdiff -Z ${name}.expected ${name}.actual
 done
